da2fb9e6efefb76a8909c15f84f5578073a13413
gitshunya <submarinecitys@gmail.com>
2024-12-22 10:19:20 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
+</template>
+
+<script setup></script>
+
+<style>
+nav {
+  margin-bottom: 20px;
+}
+</style>
